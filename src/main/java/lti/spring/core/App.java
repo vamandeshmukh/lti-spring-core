2c173eeb264ef7e21219fd27febe3631ac71b586
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-09 14:34:02 +0530

    Commit

--- src/main/java/lti/spring/core/App.java
+++ src/main/java/lti/spring/core/App.java
@@ -17,22 +17,28 @@ public class App {
 		System.out.println("Start");
 
 //		Employee emp = new Employee();
+//		Employee emp2 = new Employee();
 
 //		emp.work();
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
 
 //		Employee emp = (Employee) context.getBean("emp");
-		Employee obj = context.getBean("emp", Employee.class);
-		Employee obj2 = context.getBean("emp", Employee.class);
-
-		System.out.println(obj.toString());
-		System.out.println(obj2.toString());
-		obj.setSalary(12.5);
-		System.out.println(obj.toString());
-		System.out.println(obj2.toString());
+//		Employee obj = context.getBean("emp", Employee.class);
+//		Employee obj2 = context.getBean("emp", Employee.class);
+//
+//		System.out.println(obj.toString());
+//		System.out.println(obj2.toString());
+//		obj.setSalary(12.5);
+//		System.out.println(obj.toString());
+//		System.out.println(obj2.toString());
+//		System.out.println(obj.hashCode());
+//		System.out.println(obj2.hashCode());
 
 //		emp.work();
+
+		Employee obj3 = context.getBean("emp2", Employee.class);
+		System.out.println(obj3.toString());
 
 		((AbstractApplicationContext) context).close();
 		System.out.println("End");
