67676784b06f7d0be44a288b07d797d895d37ffd
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-09 12:40:22 +0530

    Update Employee.java

--- src/main/java/lti/spring/core/Employee.java
+++ src/main/java/lti/spring/core/Employee.java
@@ -12,6 +12,30 @@ public class Employee {
 
 	}
 
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
 	@Override
 	public String toString() {
 		return "Employee [eid=" + eid + ", firstName=" + firstName + ", salary=" + salary + "]";
