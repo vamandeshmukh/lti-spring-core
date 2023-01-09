f37754f38e40f1fd19b2d38e94628fec9976a8e6
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-09 11:25:13 +0530

    Commit

--- src/main/java/lti/spring/core/App.java
+++ src/main/java/lti/spring/core/App.java
@@ -2,14 +2,20 @@ package lti.spring.core;
 
 /**
  * 
- * @author Vaman 
+ * @author Vaman
  *
  */
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+
+		int eid = 101; // 4
+		
+		Employee emp = new Employee();
+		emp.work();
+
+	}
 }
---
+++ src/main/java/lti/spring/core/Employee.java
@@ -0,0 +1,12 @@
+package lti.spring.core;
+
+public class Employee {
+
+	// fields
+
+	void work() {
+		System.out.println("Employee works...");
+
+	}
+
+}
