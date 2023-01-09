9c371a96caebdcd9ed23ec09dc262d2fb5e38200
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-09 15:12:20 +0530

    Commit

--- src/main/java/lti/spring/core/JavaConfig.java
+++ src/main/java/lti/spring/core/JavaConfig.java
@@ -1,12 +1,13 @@
-package lti.spring.core;
-
-import org.springframework.context.annotation.Bean;
-
-public class JavaConfig {
-
-	@Bean
-	public Employee getEmployee() {
-		return new Employee(103, "Tonu", 16.75);
-	}
-
-}
+//package lti.spring.core;
+//
+//import org.springframework.context.annotation.Bean;
+//
+//public class JavaConfig {
+//
+//	@Bean
+//	public Employee getEmployee() {
+//		return new Employee(103, "Tonu", 16.75);
+//	}
+//
+//}
+////
