8d8fc2bb352692025be0d5ed3f6e9eb8d2e6c0f7
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-09 10:33:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lti-spring-core
+ Demo Spring project for LTUI trainees 9 Jan 2023
---
+++ src/main/java/lti/spring/core/App.java
@@ -0,0 +1,15 @@
+package lti.spring.core;
+
+/**
+ * 
+ * @author Vaman 
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
