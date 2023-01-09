e9290b78b215ae5122652ca792c09ab3a373b59f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-09 14:48:29 +0530

    Update App2.java

--- src/main/java/lti/spring/core/App2.java
+++ src/main/java/lti/spring/core/App2.java
@@ -1,5 +1,17 @@
 package lti.spring.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App2 {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = 
+				new AnnotationConfigApplicationContext(JavaConfig.class);
+		Employee obj = context.getBean(Employee.class);
+		System.out.println(obj.toString());
+
+	}
 
 }
